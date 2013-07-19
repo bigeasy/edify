51d58aa52f00f11a668bf731ddd0403640c284ac
Alan Gutierrez <alan@prettyrobots.com>
2013-07-18 20:10:59 -0400

    Settling on Cheerio.

--- edify.js
+++ edify.js
@@ -23,16 +23,48 @@ exports._generate = cadence(function (step, files) {
     })(files)
 })
 
-exports.generate = cadence(function (step, files) {
-    var fs =require('fs')
+exports.generate = cadence(function (step, argv) {
+    var fs = require('fs')
+    var path = require('path')
+
+    var mkdirp = require('mkdirp')
     var cheerio = require('cheerio')
 
-    step(function (file) {
-        fs.readFile(file, 'utf8', step())
-    }, function (body) {
+    var source = argv.shift()
+    var destination = path.resolve(process.cwd(), argv.shift())
+
+    // horrible location for a temporary file, but...
+    var cache = path.join(path.dirname(destination), '.' + path.basename(destination) + '.~edify')
+
+    step(function () {
+        mkdirp(path.dirname(destination), step())
+    }, function () {
+        var edify = step(function (destination) {
+            step([function (ation) {
+                fs.stat(path.join(destination, 'edify.js'), step())
+            }, function () {
+                var parent = path.dirname(destination)
+                if (parent == destination) throw new Error('cannot find edify.js')
+                return step(edify) && parent
+            }], function () {
+                return path.join(destination, 'edify.js')
+            })
+        })(1, destination)
+    }, function (module) {
+        step()(null, require(module))
+        fs.readFile(source, 'utf8', step())
+        step([function () {
+            fs.readFile(cache, 'utf8', step())
+        }, /^ENOENT$/, function () {
+            return '{}'
+        }], function (body) {
+            return JSON.parse(body)
+        })
+    }, function (edify, body, cache) {
         var $ = cheerio.load(body)    
+        edify($)
         console.log($.html())
-    })(files)
+    })
 })
 
 exports.runner = cadence(function (step, options, stdin, stdout, stderr) {
