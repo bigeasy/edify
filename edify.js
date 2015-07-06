0e0cf0a98be7a231293b48d71ac708444041d89a
Alan Gutierrez <alan@prettyrobots.com>
2015-07-06 14:54:04 -0400

    Remove dead code.

--- edify.js
+++ edify.js
@@ -1,30 +1,5 @@
 var cadence = require('cadence')
 
-//Error.stackTraceLimit = Infinity
-
-function htmlify (rawHtml) {
-    var htmlparser = require("htmlparser2");
-    var handler = new htmlparser.DefaultHandler(function (error, dom) {
-        if (error) throw error
-    });
-    var parser = new htmlparser.Parser(handler);
-    parser.parseComplete(rawHtml);
-    console.log(require('util').inspect(handler.dom, false, null))
-}
-
-exports._generate = cadence(function (step, files) {
-    var fs = require('fs')
-    var marked = require('marked')
-    var markdown = require('markdown').markdown;
-    step(function (file) {
-        fs.readFile(file, 'utf8', step())
-    }, function (body) {
-        console.log( markdown.toHTML( body ) );
-        console.log( marked( body ) );
-        //htmlify( marked( body ) );
-    })(files)
-})
-
 exports.generate = cadence(function (step, argv) {
     var fs = require('fs')
     var path = require('path')
