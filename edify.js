1da1e8cb3ef30c229cef273d9d20da80bf39b5ca
Alan Gutierrez <alan@prettyrobots.com>
2015-07-06 18:03:35 -0400

    Coverage of executable invocation.

--- edify.js
+++ edify.js
@@ -1,54 +1,59 @@
-var cadence = require('cadence')
+var cadence = require('cadence/redux')
 
-exports.generate = cadence(function (step, argv) {
+exports.generate = cadence(function (async, argv) {
     var fs = require('fs')
     var path = require('path')
 
     var mkdirp = require('mkdirp')
     var cheerio = require('cheerio')
 
-    var source = argv.shift()
+    var source = path.resolve(process.cwd(), argv.shift())
     var destination = path.resolve(process.cwd(), argv.shift())
+
+    // todo: really resolve, '..' and everything, right? Or convince yourself
+    // that it doens't matter as far as finding the `edify.js` goes, or, yeah,
+    // it does matter, very much todo.
 
     // horrible location for a temporary file, but...
     var storage = path.join(path.dirname(destination), '.' + path.basename(destination) + '.edify~')
 
-    step(function () {
-        mkdirp(path.dirname(destination), step())
+    async(function () {
+        mkdirp(path.dirname(destination), async())
     }, function () {
-        var edify = step(function (destination) {
-            step([function (ation) {
-                fs.stat(path.join(destination, 'edify.js'), step())
-            }, function () {
-                var parent = path.dirname(destination)
-                if (parent == destination) throw new Error('cannot find project\'s edify.js')
-                return step(edify) && parent
+        var loop = async(function (directory) {
+            async([function () {
+                fs.stat(path.join(directory, 'edify.js'), async())
+            }, /^ENOENT$/, function () {
+                if (directory == '/') throw new Error('cannot find "edify.js"')
+                return [ loop(), path.dirname(directory) ]
             }], function () {
-                return path.join(destination, 'edify.js')
+                return [ loop, path.join(directory, 'edify.js') ]
             })
-        })(1, destination)
+        })(path.dirname(source))
     }, function (module) {
-        step()(null, require(module))
-        fs.readFile(source, 'utf8', step())
-        step([function () {
-            fs.readFile(storage, 'utf8', step())
-        }, /^ENOENT$/, function () {
-            return '{}'
-        }], function (body) {
-            return JSON.parse(body)
-        })
-    }, function (edify, body, cache) {
-        var $ = cheerio.load(body)    
-        step(function () {
-            edify($, cache, step())
-        }, function () {
-            fs.writeFile(destination, $.html(), 'utf8', step())
-            fs.writeFile(storage, JSON.stringify(cache, null, 2),  'utf8', step())
+        var edify = require(module)
+        async(function () {
+            fs.readFile(source, 'utf8', async())
+            async([function () {
+                fs.readFile(storage, 'utf8', async())
+            }, /^ENOENT$/, function () {
+                return '{}'
+            }], function (body) {
+                return JSON.parse(body)
+            })
+        }, function (body, cache) {
+            var $ = cheerio.load(body)
+            async(function () {
+                edify($, cache, async())
+            }, function () {
+                fs.writeFile(destination, $.html(), 'utf8', async())
+                fs.writeFile(storage, JSON.stringify(cache, null, 2),  'utf8', async())
+            })
         })
     })
 })
 
-exports.runner = cadence(function (step, options, stdin, stdout, stderr) {
+exports.runner = cadence(function (async, options, stdin, stdout, stderr) {
     if (options.params.help) options.help()
-    exports.generate(options.argv, step())
+    exports.generate(options.argv, async())
 })
---
+++ t/fixtures/project/edify.js
@@ -0,0 +1,4 @@
+module.exports = function ($, cache, callback) {
+    $('html').text('Hello, World!')
+    callback()
+}
