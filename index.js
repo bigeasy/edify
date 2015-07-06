8939360b4b21daf8e0531d3986ab02bf8d6a6fcb
Alan Gutierrez <alan@prettyrobots.com>
2015-07-06 14:55:57 -0400

    Convert markdown to Cadence Redux.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var cadence = require('cadence')
+var redux = require('cadence/redux')
 var spawn = require('child_process').spawn
 
 // make a note, I like to use cadence even when the actual
 // implementation is not evented.
-exports.marked = cadence(function (step, $, selector) {
+exports.marked = redux(function (step, $, selector) {
     var marked = require('marked')
     $(selector).each(function () {
         this.html(marked(this.text()))
