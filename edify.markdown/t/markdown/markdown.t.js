c1ea993b1eb6339dff108caedf48fb614c8642e7
Alan Gutierrez <alan@prettyrobots.com>
2016-05-10 22:52:27 -0500

    Create Markdown test.

--- edify.markdown/t/markdown/markdown.t.js
+++ edify.markdown/t/markdown/markdown.t.js
@@ -1,13 +1,13 @@
 require('proof')(1, require('cadence')(prove))
 
 function prove (async, assert) {
-    var bin = require('../../markdown.bin'), 'require')
+    var bin = require('../../markdown.bin')
     var io
     async(function () {
-        io = bin({}, [], {}, async())
-        io.stdin.write('*strong*')
+        io = bin({}, [ '--select', '.markdown' ], {}, async())
+        io.stdin.write('<div class="markdown">*strong*</div>')
         io.stdin.end()
     }, function () {
-        assert(io.stdout.read().toString(), 'Wink API Poster API', 'running')
+        assert(io.stdout.read().toString(), '<div class="markdown"><p><em>strong</em></p>\n</div>', 'running')
     })
 }
