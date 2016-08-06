7570f280310de96ec0370f1d7ab6c2dc1cf6f056
Alan Gutierrez <alan@prettyrobots.com>
2016-08-06 00:43:06 -0500

    Implement an include command.
    Closes #38.

---
+++ edify.include/include.bin.js
@@ -0,0 +1,49 @@
+/*
+
+    ___ usage ___ en_US ___
+    node markdown.bin.js <options> [sockets directory, sockets directory...]
+
+    options:
+
+        --help                          display help message
+        -s, --select        <string>    path to select
+        -t, --type          <string>    either text or html
+
+    ___ $ ___ en_US ___
+
+        select is required:
+            the `--select` argument is a required argument
+
+    ___ . ___
+
+ */
+require('arguable')(module, require('cadence')(function (async, program) {
+    var delta = require('delta')
+    var path = require('path')
+    var cheerio = require('cheerio')
+    var fs = require('fs')
+
+    program.helpIf(program.ultimate.help)
+    program.required('select', 'type')
+    program.assert(/^(?:text|html)$/.test(program.ultimate.type), 'bad type')
+
+    async(function () {
+        program.stdin.resume()
+        delta(async()).ee(program.stdin).on('data', []).on('end')
+    }, function (lines) {
+        var $ = cheerio.load(Buffer.concat(lines).toString('utf8'))
+        async(function () {
+            async.forEach(function (selected) {
+                async(function () {
+                    var file = $(selected).attr('data-file')
+                    var resolved = path.resolve(__dirname, file)
+                    fs.readFile(resolved, 'utf8', async())
+                }, function (body) {
+                    $(selected)[program.ultimate.type](body)
+                })
+            })($(program.ultimate.select))
+        }, function () {
+            program.stdout.write($.html())
+        })
+    })
+}))
---
+++ edify.include/t/include.bin.t.js
@@ -0,0 +1,13 @@
+require('proof')(1, require('cadence')(prove))
+
+function prove (async, assert) {
+    var bin = require('../include.bin')
+    var io
+    async(function () {
+        io = bin([ '--select', '.include', '--type', 'text' ], {}, async())
+        io.stdin.write('<div data-file="t/include.txt" class="include"></div>')
+        io.stdin.end()
+    }, function () {
+        assert(io.stdout.read().toString(), '<div data-file="t/include.txt" class="include">DT&amp;I\n</div>', 'running')
+    })
+}
