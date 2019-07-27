83051695a89872348eae109513eb6fed1ead965c
Alan Gutierrez <alan@prettyrobots.com>
2019-07-27 18:32:54 -0500

    Convert root executable to `async`/`await`.

--- edify.root/edify.bin.js
+++ edify.root/edify.bin.js
@@ -18,5 +18,8 @@
 */
 require('arguable')(module, async arguable => {
     arguable.helpIf(arguable.ultimate.help)
-    arguable.delegate(require, 'edify.%s', arguable.argv.shift())
+    const delegate = arguable.delegate(require, 'edify.%s', arguable.argv.shift())
+    const child = delegate(arguable.argv)
+    arguable.destroyed.then(child.destroy.bind(child))
+    return await child.promise
 })
--- edify.root/test/edify.bin.test.js
+++ edify.root/test/edify.bin.test.js
@@ -1,6 +1,8 @@
 describe('edify', () => {
+    const assert = require('assert')
     const edify = require('../edify.bin')
     it('can launch', async () => {
         const child = edify([ 'test' ])
+        assert.equal(await child.promise, 0, 'exit')
     })
 })
