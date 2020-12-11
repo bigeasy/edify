b354f035f59146937635092227efbc01b8d40d62
Alan Gutierrez <alan@prettyrobots.com>
2020-12-11 05:15:32 -0600

    Upgrade `arguable` to `12.0.3`.
    Arguable 12.0.3 renamed `promise` to `exit`.
    
    https://github.com/bigeasy/arguable/releases/tag/v12.0.3
    
    Closes #93.

--- edify.highlight/test/highlight.bin.test.js
+++ edify.highlight/test/highlight.bin.test.js
@@ -7,6 +7,6 @@ require('proof')(2, async okay => {
     })
     child.options.$stdin.write('<pre class="javascript">var i = 0;</div>')
     child.options.$stdin.end()
-    okay(await child.promise, 0, 'exit')
+    okay(await child.exit, 0, 'exit')
     okay(child.options.$stdout.read().toString(), '<html><head></head><body><pre class="javascript"><span class="hljs-keyword">var</span> i = <span class="hljs-number">0</span>;</pre></body></html>', 'highlight')
 })
